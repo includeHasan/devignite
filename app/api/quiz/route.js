d90d52d19020e9a130470ccacf693ba6f6a60bf8
Ahmed Siddiqui <37452138+siddAhmed@users.noreply.github.com>
2024-03-02 15:19:28 +0000

    feat: add quiz api endpoint

---
+++ app/api/quiz/route.js
@@ -0,0 +1,50 @@
+import { createClient } from "@supabase/supabase-js";
+
+const db = createClient(
+    process.env.connection_string,
+    process.env.supabase_key
+);
+
+function getRandomQuestions(data) {
+    const subjects = {};
+
+    // Group questions by subject
+    data.forEach(item => {
+        if (!subjects[item.subject]) {
+            subjects[item.subject] = [];
+        }
+        subjects[item.subject].push(item.question);
+    });
+
+    const result = {};
+
+    // Select 3 random questions for each subject
+    for (const subject in subjects) {
+        result[subject] = [];
+        const questions = subjects[subject];
+        const shuffled = questions.sort(() => 0.5 - Math.random());
+        result[subject] = shuffled.slice(0, 5);
+    }
+
+    return result;
+}
+
+export async function POST(req, res) {
+    const params = await req.json();
+
+    try {
+        let { data: quiz_list, error } = await db
+            .from('quiz')
+            .select("*")
+            .eq("isAdult", params.isAdult)
+
+        // Get random questions
+        const randomQuestions = getRandomQuestions(quiz_list);
+        console.log(randomQuestions);
+
+        return Response.json({ success: true, random_quiz_questions: randomQuestions });
+    } catch (error) {
+        console.error(error);
+        return Response.json({ success: false, message: 'Internal Server Error' });
+    }
+}
